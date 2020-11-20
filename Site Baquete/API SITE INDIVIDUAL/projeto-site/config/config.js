4a0b0d1dd379019fadd778aeba47c7a5a24e34a9
felipe-olegario <felipe.olegario@bandtec.com.br>
2020-11-20 00:59:52 -0200

    atualizado

--- Site Baquete/API SITE INDIVIDUAL/projeto-site/config/config.js
+++ Site Baquete/API SITE INDIVIDUAL/projeto-site/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   production: {
-    username: 'SEU_USUARIO_DO_BANCO_DE_DADOS',
-    password: 'SUA_SENHA_DO_BANCO_DE_DADOS',
-    database: 'NOME_DO_SEU_BANCO_DE_DADOS',
-    host: 'URL_DO_SEU_BANCO_DE_DADOS.database.windows.net',
+    username: 'felipe-olegario',
+    password: 'Laurinha070700',
+    database: 'lakers',
+    host: 'lakers.database.windows.net',
     dialect: 'mssql',
-    xuse_env_variable: 'DATABASE_URL',
+    xuse_env_variable: 'lakers',
     dialectOptions: {
       options: {
         encrypt: true
@@ -20,4 +20,3 @@ module.exports = {
     }
   }
 };
- 
